a70432d7edb54c1eea70478744e4ae7bc8b4f879
cimem37 <76502749+cimem37@users.noreply.github.com>
2023-03-06 11:13:53 +0000

    Created method for BMI based on the given formula
    Takes input height / weight then BMI = (weight in kg) / (height in metres)2 and gives BMI score output

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class BMI {
+    static void Main(string[] args) {
+        Console.WriteLine("- - - - - - - - - - - - - - - - - - - - -");
+        Console.WriteLine("BMI *Body Mass Index* Calculator By Bruno");
+        Console.WriteLine("- - - - - - - - - - - - - - - - - - - - -");
+
+        Console.WriteLine("Please enter your height ( in cm ):");
+        double height = Convert.ToDouble(Console.ReadLine()) / 100; // Convert height to meters
+
+        Console.WriteLine("Please enter your weight ( in kg ):");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        double bmi = weight / (height * height); // Calculate BMI
+
+        Console.WriteLine("Your BMI accordingly is : " + bmi);
+    }
+}
